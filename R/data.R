5533a3e19db060bda3faf0f5a1d85f8bc0a00626
Unterfinger Merlin (NMD) <merlin.unterfinger@sbb.ch>
2019-10-05 22:41:52 +0200

    Add package data

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' Sample Data: Addresses
+#'
+#' Some addresses inside the boundaries of the Zurich city, Switzerland.
+#'
+#' @docType data
+#'
+#' @usage data(address)
+#'
+#' @format An object of class \code{"character"}.
+#'
+#' @keywords datasets
+#'
+#' @source \code{save(address, file="data/address.RData")}
+#'
+#' @examples
+#' data(address)
+"address"
+
+#' Sample Data: POIs
+#'
+#' Geocoded Points of Interest (POIs) inside the boundaries of the Zurich city,
+#' Switzerland.
+#'
+#' @docType data
+#'
+#' @usage data(poi)
+#'
+#' @format An object of class \code{"character"}.
+#'
+#' @keywords datasets
+#'
+#' @source \code{save(poi, file="data/poi.RData")}
+#'
+#' @examples
+#' data(poi)
+"poi"
