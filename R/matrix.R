9d3393f6f1eff516051ee02a639fe9fb45939b62
Unterfinger Merlin (NMD) <merlin.unterfinger@sbb.ch>
2019-10-05 22:39:50 +0200

    Binding to the Routing API, matrix

---
+++ R/matrix.R
@@ -0,0 +1,155 @@
+#' HERE Routing API: Matrix
+#'
+#' Calcuates a matrix of route summaries.
+#'
+#' @references
+#' \href{https://developer.here.com/documentation/routing/topics/resource-calculate-matrix.html}{HERE Routing API: Calculate Matrix}
+#'
+#' @param start
+#' @param destination
+#' @param type
+#' @param mode
+#' @param traffic
+#' @param searchRange
+#' @param attribute
+#' @param departure
+#'
+#' @return
+#' @export
+#'
+#' @examples
+route_matrix <- function(start, destination = start, type = "fastest", mode = "car",
+                         traffic = FALSE, searchRange = 10000,
+                         attribute = c("distance", "traveltime"),
+                         departure = NULL) {
+  # Checks
+  .check_points(start)
+  .check_points(destination)
+  .check_datetime(departure)
+  .check_attributes(attribute)
+  .check_type(type = type, request = "calculatematrix")
+  .check_mode(mode = mode, request = "calculatematrix")
+
+  # CRS transformation and formatting
+  start <- sf::st_coordinates(
+    sf::st_transform(start, 4326)
+  )
+  start <- paste0(
+    "geo!", start[, 2], ",", start[, 1]
+  )
+  destination <- sf::st_coordinates(
+    sf::st_transform(destination, 4326)
+  )
+  destination <- paste0(
+    "geo!", destination[, 2], ",", destination[, 1]
+  )
+
+  # Add authentification
+  url <- .add_auth(
+    url = "https://matrix.route.api.here.com/routing/7.2/calculatematrix.json?"
+  )
+
+  # Switch coordinates to use max request size of 15x100
+  if (length(start) > length(destination)) {
+    switch <- TRUE
+    start_tmp <- start
+    start <- destination
+    destination <- start_tmp
+  } else {
+    switch <- FALSE
+  }
+
+  # Create batches, indices and format coordinates
+  batch_size_start <- 15
+  batch_size_dest <- 100
+  start_div <- seq(0, length(start), batch_size_start)
+  dest_div <- seq(0, length(destination), batch_size_dest)
+  start_idx <- list()
+  dest_idx <- list()
+
+  coords <- sapply(X = start_div, FUN = function(i) {
+    start_batch <- start[(i + 1):(i + batch_size_start)]
+    start_batch <- start_batch[!is.na(start_batch)]
+    for (j in dest_div) {
+      dest_batch <- destination[(j + 1):(j + batch_size_dest)]
+      dest_batch <- dest_batch[!is.na(dest_batch)]
+      start_idx <<- append(start_idx, list(seq(0 + i, length(start_batch) - 1 + i, 1)))
+      dest_idx <<- append(dest_idx, list(seq(0 + j, length(dest_batch) - 1 + j, 1)))
+      return(paste0("&",
+             paste0("start",
+                    seq(0, length(start_batch) - 1, 1), "=",
+                    start_batch,
+                    collapse = "&"), "&",
+             paste0("destination",
+                    seq(0, length(dest_batch) - 1, 1), "=",
+                    dest_batch,
+                    collapse = "&")
+      ))
+    }
+  })
+
+  # Add start coords
+  url = paste0(
+    url,
+    coords
+  )
+
+  # Add mode
+  url = .add_mode(
+    url = url,
+    type = type,
+    mode = mode,
+    traffic = traffic
+  )
+
+  # Add searchRange
+  url = paste0(
+    url,
+    "&searchRange=",
+    searchRange
+  )
+
+  # Add departure time
+  url <- .add_departure(
+    url,
+    departure
+  )
+
+  # Add summaryAttributes
+  url = paste0(
+    url,
+    "&summaryAttributes=",
+    paste0(attribute, collapse = ",")
+  )
+
+  # Request and get content
+  data <- .get_content(
+    url = url
+  )
+
+  # Extract information
+  count <- 1
+  routes <- data.table::rbindlist(
+    lapply(data, function(con) {
+      df <- jsonlite::fromJSON(con)$response$matrixEntry
+      # Retrieve original indices
+      df$startIndex <- start_idx[[count]][df$startIndex + 1] + 1
+      df$destinationIndex <- dest_idx[[count]][df$destinationIndex + 1] + 1
+      count <<- count + 1
+      # Flatten columns
+      cbind(df[, 1:2], df$summary)
+    })
+  )
+
+  # Switch back indices
+  if (switch) {
+    tmp <- routes$startIndex
+    routes$startIndex <- routes$destinationIndex
+    routes$destinationIndex <- tmp
+  }
+
+  # Reorder
+  routes <- routes[order(routes$startIndex,
+                         routes$destinationIndex), ]
+  return(routes)
+}
