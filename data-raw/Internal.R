425f05d60e25893871726a349514f31288a20c81
Merlin Unterfinger <info@munterfinger.ch>
2020-08-29 16:50:27 +0200

    Update package examples and API mocks.

--- data-raw/Internal.R
+++ data-raw/Internal.R
@@ -6,10 +6,8 @@ url_geocode <-
   geocode(addresses = poi$city, url_only = TRUE)
 url_autocomplete <-
   autocomplete(addresses = poi$city, results = 3, url_only = TRUE)
-url_reverse_geocode_addresses <-
-  reverse_geocode(poi = poi, landmarks = FALSE, results = 3, url_only = TRUE)
-url_reverse_geocode_landmarks <-
-  reverse_geocode(poi = poi, landmarks = TRUE, results = 3, url_only = TRUE)
+url_reverse_geocode <-
+  reverse_geocode(poi = poi, results = 3, url_only = TRUE)
 url_route <-
   route(origin = poi[1:2, ], destination = poi[3:4, ], url_only = TRUE)
 url_route_matrix <-
@@ -39,8 +37,7 @@ url_station <-
 mock <- list(
   geocode_response = hereR:::.get_content(url_geocode),
   autocomplete_response = hereR:::.get_content(url_autocomplete),
-  reverse_geocode_addresses = hereR:::.get_content(url_reverse_geocode_addresses),
-  reverse_geocode_landmarks = hereR:::.get_content(url_reverse_geocode_landmarks),
+  reverse_geocode = hereR:::.get_content(url_reverse_geocode),
   route_response = hereR:::.get_content(url_route),
   route_matrix_response = hereR:::.get_content(url_route_matrix),
   intermodal_route_response = hereR:::.get_content(url_intermodal_route),
@@ -59,8 +56,7 @@ mock <- list(
 example <- list(
   geocode = geocode(addresses = poi$city),
   autocomplete = autocomplete(addresses = poi$city, results = 3),
-  reverse_geocode_addresses = reverse_geocode(poi = poi, results = 3, landmarks = FALSE),
-  reverse_geocode_landmarks = reverse_geocode(poi = poi, results = 3, landmarks = TRUE),
+  reverse_geocode_addresses = reverse_geocode(poi = poi, results = 3),
   route = route(origin = poi[1:2, ], destination = poi[3:4, ]),
   route_matrix = route_matrix(origin = poi),
   intermodal_route = intermodal_route(origin = poi[1:3, ], destination = poi[4:6, ]),
