70e0c6745656910e800bb368a64f310a40ddfaaa
Merlin Unterfinger <info@munterfinger.ch>
2022-06-30 17:55:14 +0200

    Request tolls for routes with transport modes `"car"`, `"truck"`, `"taxi"` and `"bus"` in `route()`, closes #151.

--- tests/testthat/test-route.R
+++ tests/testthat/test-route.R
@@ -11,11 +11,18 @@ test_that("route works", {
   expect_error(route(origin = poi, destination = poi, transport_mode = "not_a_transport_mode"))
   expect_error(route(origin = poi, destination = poi, routing_mode = "not_a_routing_mode"))
   expect_error(route(origin = poi, destination = poi, traffic = "not_a_bool"), "'traffic' must be a 'boolean' value.")
+  expect_error(route(origin = poi, destination = poi, vignettes = "not_a_bool"), "'vignettes' must be a 'boolean' value.")
   expect_error(route(origin = poi, destination = poi, url_only = "not_a_bool"), "'url_only' must be a 'boolean' value.")
 
   # Avoid area and features
   expect_true(all(grepl("&avoid[areas]=bbox:", route(origin = poi, destination = poi, avoid_area = aoi, url_only = TRUE), fixed = TRUE)))
   expect_true(all(grepl("&avoid[features]=tollRoad,ferry", route(origin = poi, destination = poi, avoid_feature = c("tollRoad", "ferry"), url_only = TRUE), fixed = TRUE)))
+
+  # Request tolls
+  expect_true(all(grepl(",tolls&tolls[summaries]=total", route(origin = poi, destination = poi, vignettes = TRUE, url_only = TRUE), fixed = TRUE)))
+  expect_true(all(grepl(",tolls&tolls[summaries]=total&tolls[vignettes]=all", route(origin = poi, destination = poi, vignettes = FALSE, url_only = TRUE), fixed = TRUE)))
+  expect_false(all(grepl(",tolls&tolls[summaries]=total", route(origin = poi, destination = poi, transport_mode = "bicycle", url_only = TRUE), fixed = TRUE)))
+  expect_false(all(grepl(",tolls&tolls[summaries]=total&tolls[vignettes]=all", route(origin = poi, destination = poi, transport_mode = "bicycle", vignettes = FALSE, url_only = TRUE), fixed = TRUE)))
 
   # Test with API response mock
   with_mock(
